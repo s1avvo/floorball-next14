e6bc8e5eb3b7c4570c77ae8f55acebffcfd32f10
s1avo <s.nadolny@hotmail.com>
2023-11-05 16:10:32 +0100

    feat(footer): mobile screen view

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
 	return (
-		<footer className="space-y-5 bg-amber-400 p-5 px-2 sm:px-8 md:px-56">
+		<footer className="space-y-5 bg-amber-400 p-5 px-2 sm:px-8 md:px-24 xl:px-56">
 			<h3>Trenerzy:</h3>
 			<div className="flex w-full flex-wrap gap-y-2">
 				<div className="basis-1/2 sm:basis-1/4">
