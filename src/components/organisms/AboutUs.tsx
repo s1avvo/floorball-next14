40abedd41e8ce00b4e93108f09faf203af91f8d2
s1avo <s.nadolny@hotmail.com>
2023-11-05 16:06:40 +0100

    feat(about-us): mobile screen view

--- src/components/organisms/AboutUs.tsx
+++ src/components/organisms/AboutUs.tsx
@@ -6,15 +6,15 @@ export const AboutUs = () => {
 		<section className="relative flex h-full min-h-screen w-full flex-col" id="about-us">
 			{/* text */}
 			<article>
-				<div className="flex flex-col px-4 pt-28 sm:ps-20 md:ps-48">
-					<div className="ps-36 text-3xl font-semibold text-blue-900 sm:ps-20 sm:text-5xl md:ps-56 md:text-8xl">
+				<div className="flex flex-col ps-10 pt-28 sm:ps-20 xl:ps-48">
+					<div className="ps-36 text-3xl font-semibold text-blue-900 sm:ps-20 sm:text-5xl xl:ps-56 xl:text-8xl">
 						FLOORBALL
 					</div>
-					<div className="md:text-12xl text-[7rem]/[120px] font-extrabold text-cyan-500 sm:text-9xl">
+					<div className="xl:text-12xl text-[7rem]/[120px] font-extrabold text-cyan-500 sm:text-9xl">
 						ŚREM
 					</div>
-					<div className="flex max-w-[250px] flex-col gap-8 sm:max-w-[300px] md:max-w-md">
-						<div className="mt-8 hidden border-y-2 border-blue-900 px-6 py-4 italic md:flex">
+					<div className="flex max-w-[250px] flex-col gap-8 sm:max-w-[300px] xl:max-w-md">
+						<div className="mt-8 hidden border-y-2 border-blue-900 px-6 py-4 italic xl:flex">
 							<p>
 								<q className="text-sm">
 									Cena sukcesu to ciężka praca, poświęcenie i determinacja, by niezależnie od tego,
