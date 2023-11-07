b6b9ef9ab146e62529170033ce3007dc51cb2003
s1avo <s.nadolny@hotmail.com>
2023-11-07 20:33:15 +0100

    feat(constants): constants data

---
+++ src/constants/trening.ts
@@ -0,0 +1,12 @@
+export const trening = [
+	{
+		title: "1 | 2",
+		content: "17:30 - 18:30",
+		image: "/assets/unihokphoto-1.webp",
+	},
+	{
+		title: "3 | 4 | 5",
+		content: "16:30 - 17:30",
+		image: "/assets/unihokphoto-1.webp",
+	},
+];
