48973d85b44ff7a38c6088ff6febc8e99a19f131
s1avo <s.nadolny@hotmail.com>
2023-11-29 19:57:40 +0100

    fix(): resend error

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,4 +1,6 @@
 "use server";
+import type React from "react";
+import { renderAsync } from "@react-email/render";
 import { Resend } from "resend";
 import { FloorballEmailTemplate } from "@/components/atoms/EmailTemplate";
 
@@ -10,11 +12,15 @@ export const sendMessageAction = async (formData: FormData) => {
 	const subject = String(formData.get("subject"));
 	const message = String(formData.get("message"));
 
+	const html = await renderAsync(
+		FloorballEmailTemplate({ name, email, subject, message }) as React.ReactElement,
+	);
+
 	const { data } = await resend.emails.send({
 		from: "Floorballsrem.com <no-reply@floorballsrem.com>",
-		to: ["arkadiusz.nadolny@gmail.com"],
+		to: ["s.nadolny@hotmail.com"],
 		subject: subject,
-		react: FloorballEmailTemplate({ name, email, subject, message }),
+		html: html,
 	});
 
 	return data;
