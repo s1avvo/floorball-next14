406bcfee7dadaf86f49be75a40152796dd7ef831
s1avo <s.nadolny@hotmail.com>
2023-12-04 19:34:04 +0100

    ref(): refactoring

--- src/app/api/news/route.ts
+++ src/app/api/news/route.ts
@@ -1,23 +1,29 @@
-import { NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
+// import { NextResponse } from "next/server";
 import { revalidatePath } from "next/cache";
+import prisma from "../client";
 
 export async function POST(request: Request) {
-	const prisma = new PrismaClient();
 	const body = await request.json();
-
 	const { title, first_paragraph, second_paragraph, link } = body;
 
 	try {
-		const post = await prisma.post.create({
-			data: { title, first_paragraph, second_paragraph, link },
-		});
+		if (body && title.length !== 0 && first_paragraph !== 0) {
+			const post = await prisma.post.create({
+				data: { title, first_paragraph, second_paragraph, link },
+			});
 
-		revalidatePath("/");
-		console.log("request ok");
-		return NextResponse.json(post, { status: 200 });
+			revalidatePath("/");
+
+			console.log("request ok", post.id);
+
+			return new Response(post.id, { status: 201 });
+		} else {
+			return new Response("data error", { status: 400 });
+		}
+		// return NextResponse.json(post, { status: 200 });
 	} catch (error) {
 		console.error("request error", error);
-		NextResponse.json({ error: "error creating news" }, { status: 500 });
+		return new Response("error creating news", { status: 500 });
+		// NextResponse.json({ error: "error creating news" }, { status: 500 });
 	}
 }
