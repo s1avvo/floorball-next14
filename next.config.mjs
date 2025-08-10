163a28cf2574ca815993704d82a8e5dc3d74f9eb
s1avo <s.nadolny@hotmail.com>
2025-08-10 14:43:13 +0200

    fix: ignor eslint during build

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,12 @@ import createMDX from "@next/mdx";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	reactStrictMode: true,
+
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+
 	pageExtensions: ["ts", "tsx", "mdx"],
 
 	images: {
