0d0f5b1dc622d6978716d0df27ea94b24a051e25
s1avo <s.nadolny@hotmail.com>
2025-08-10 11:48:50 +0200

    fix: update parameter handling

--- src/app/(static-pages)/[filename]/page.tsx
+++ src/app/(static-pages)/[filename]/page.tsx
@@ -5,12 +5,13 @@ export const generateStaticParams = () => {
 	return [{ filename: "polityka-prywatnosci" }];
 };
 
-export default async function StaticPage({ params }: { params: { filename: string } }) {
-	const Page = await import(`./${params.filename}.mdx`).then(
+export default async function StaticPage(props: { params: Promise<{ filename: string }> }) {
+    const params = await props.params;
+    const Page = await import(`./${params.filename}.mdx`).then(
 		(module: { default: ComponentType }) => module.default,
 		() => notFound(),
 	);
-	return (
+    return (
 		<article className="prose prose-sm max-w-none px-6 text-start tracking-tight sm:prose-base sm:text-justify">
 			<Page />
 		</article>
